215759d5b82c2ad0027aa8c478233c0ae0248518
Unknown <baptiste.viala@outlook.com>
2017-08-11 19:41:54 +0200

    Can't log in when already logged in

--- routes/login.js
+++ routes/login.js
@@ -2,10 +2,6 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-router.get('/', function(req, res) {
-    res.render('login', { user : req.user });
-});
-
 router.post('/', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
 });
