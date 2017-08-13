8c3e4a01608cf58224bac994a54b245128045cf9
Unknown <baptiste.viala@outlook.com>
2017-08-13 19:48:51 +0200

    Add error message feedback for login failures

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-    title: 'Nodeinit',
-    user: req.user
+      title: 'Nodeinit',
+      user: req.user,
+      message: req.flash('error')
   });
 });
 
--- routes/login.js
+++ routes/login.js
@@ -2,8 +2,14 @@ var express = require('express');
 var router = express.Router();
 var passport = require('passport');
 
-router.post('/', passport.authenticate('local'), function(req, res) {
+/*router.post('/', passport.authenticate('local'), function(req, res) {
     res.redirect('/');
-});
+});*/
+
+router.post('/', passport.authenticate('local', {
+    successRedirect: '/',
+    failureRedirect: '/',
+    failureFlash: true
+}));
 
 module.exports = router;
