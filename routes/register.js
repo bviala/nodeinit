7873cad821767aaa9d3f7af4efe398bf18922fe4
Unknown <baptiste.viala@outlook.com>
2017-08-13 20:07:15 +0200

    Add error message feedback for registration failure

--- routes/register.js
+++ routes/register.js
@@ -14,7 +14,10 @@ router.get('/', function(req, res) {
 router.post('/', function(req, res) {
     Account.register(new Account({ username : req.body.username }), req.body.password, function(err, account) {
         if (err) {
-            return res.render('register', { account : account });
+            return res.render('register', {
+                account : account,
+                message: err.message
+            });
         }
 
         passport.authenticate('local')(req, res, function () {
